c0a1e7b8b29a74e207fd5c0b3ebd017fa6a91216
VindSent <vincentvanhintum@gmail.com>
2025-12-16 11:29:59 +0100

    Custom application PoC

--- libs/amiga_package/ops.py
+++ libs/amiga_package/ops.py
@@ -1,8 +1,2 @@
-"""Template module with math functions."""
-
-
-def add(a: int, b: int) -> int:
-    """Template function to add to integer values."""
-    assert isinstance(a, int), f"not an integer. Got: {type(a)}"
-    assert type(a) == type(b), f"Type of 'b' must be equal. Got: {type(b)}"
-    return a + b
+def increment(value: int) -> int:
+    return value + 1
